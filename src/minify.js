26455eadc077ebdc1deb73f37987edb93f83245c
4424dev <50632278+4424dev@users.noreply.github.com>
2020-03-05 13:45:40 -0500

    Update minify.js

--- src/minify.js
+++ src/minify.js
@@ -1,13 +1,25 @@
 module.exports = {
-    minify: function(code, options = {removeAttributeQuotes: true, removeOptionalTags: true, collapseBooleanAttributes: true, collapseInlineTagWhitespace: true, collapseWhitespace: true, continueOnParseError: true, decodeEntities: true, minifyCSS: true, minifyJS: true, removeComments: true, quoteCharacter: "\"", removeEmptyAttributes: true, useShortDoctype: true, sortAttributes: true, sortClassName: true, removeScriptTypeAttributes: true, removeStyleLinkTypeAttributes: true, removeRedundantAttributes: true}) {
-        const detect = require("lang-detector");
-        const JavaScriptTerser = require("terser");
-        const HTMLTerser = require("html-minifier-terser");
-        switch (detect(code)) {
+    minify: function(code, options) {
+        const Detector = require("lang-detector");
+        const JS = require("terser");
+        const HTML = require("html-minifier-terser");
+        const CSS = require("clean-css");
+        switch (Detector(code)) {
             case "JavaScript":
-                return JavaScriptTerser.minify(code);
+                if (typeof options == "undefined") {
+                    options = {};
+                }
+                return JS.minify(code, options)[Object.keys(JS.minify(code, options))[0]];
             case "HTML":
-                return HTMLTerser.minify(code, options);
+                if (typeof options == "undefined") {
+                    options = {removeAttributeQuotes: true, removeOptionalTags: true, collapseBooleanAttributes: true, collapseInlineTagWhitespace: true, collapseWhitespace: true, continueOnParseError: true, decodeEntities: true, minifyCSS: true, minifyJS: true, removeComments: true, quoteCharacter: "\"", removeEmptyAttributes: true, useShortDoctype: true, sortAttributes: true, sortClassName: true, removeScriptTypeAttributes: true, removeStyleLinkTypeAttributes: true, removeRedundantAttributes: true};
+                }
+                return HTML.minify(code, options);
+            case "CSS":
+                if (typeof options == "undefined") {
+                    options = "";
+                }
+                return new CSS(options).minify(code)[Object.keys(new CSS(options).minify(code))[0]];
         }
     }
 };
